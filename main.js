1c9ba80588ae04eff99222595b6b0d2bd8968404
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-01 13:09:32 -0400

    tasks

---
+++ main.js
@@ -0,0 +1,37 @@
+// console.log average price of items
+
+
+
+
+
+//Show me how to get an array of items that cost between $14.00 and $18.00 USD
+
+
+
+
+
+
+// Show me how find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
+
+
+
+
+
+
+// Show me how to find which items are made of wood. Please console.log the ones you find.
+
+
+
+
+
+
+
+// Show me how to find which items are made of eight or more materials. Please console.log the ones you find.
+
+
+
+
+
+
+
+// Show me how to calculate how many items were made by their sellers
