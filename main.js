e0671a72bafe758e654c9fe6584a095fff7672fd
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-02 00:14:41 -0400

    working the problem

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-// console.log average price of items
+ console.log(items)
+ // console.log average price of items
+var allPrices = price.map[]
 
+
+
+ //gets number of items
+
+var numItems = items.length;
+
+//average price of items
+var sum = allPrices/numItems;
+
+var sumAverage sum (parseIntg)
+
+console.log(sumaverage)
 
 
 
 
 //Show me how to get an array of items that cost between $14.00 and $18.00 USD
+// if item >14 but <18 print out console.log
+
+var plusItems = function isMoreThan(price) {
+  return element >= 14;
+
+}
+
+var minusItems = function isLessThan(price) {
+  return element <=18:
+
+}
+
+console.log(plusItems+minusItems)
+
 
 
 
@@ -12,26 +40,42 @@
 
 
 // Show me how find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
+// if item = gbp then console.log &pound \u00A3
 
 
-
+//var array = list items
 
 
 
 // Show me how to find which items are made of wood. Please console.log the ones you find.
+// when items materials = wood console.log
 
-
-
-
-
+//var materials = itemsMaterials
+items.filter(function (it){
+  if ("wood" === it.materials){
+    return true;
+  } else {
+      return false;
+      }
+})
 
 
 // Show me how to find which items are made of eight or more materials. Please console.log the ones you find.
-
-
+// when items materials <= 8
 
 
 
 
 
 // Show me how to calculate how many items were made by their sellers
+// if items ==="i-did" console.log
+// var I_Did = document.getElementById.length
+//     console.log(I_Did)
+
+items.filter( function (it){
+  if ('i_did' === it.who_made) {
+    return true;
+  } else {
+      return false;
+  }
+})
